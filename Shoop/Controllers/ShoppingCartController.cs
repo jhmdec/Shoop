5ef9c7c5f27fb184cdddb0a5c65a4bf723d4de75
Jacqueline de Caluwe <jhmdec@hotmail.com>
2020-08-13 16:01:43 +0200

    First parts shopping cart

---
+++ Shoop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shoop.Models;
+
+namespace Shoop.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult AddMovieCopy()
+        {
+            return View();
+        }
+
+        public ActionResult RemoveMovieCopy()
+        {
+            return View();
+        }
+        public ActionResult Buy()
+        {
+            return View();
+        }
+    }
+}
---
+++ Shoop/Models/ShoppingCartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Shoop.Models;
+
+
+namespace Shoop.Models
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Movie> CartOrder { get; set; }
+        public int NrOfMovieItems { get; set; }
+    }
+}

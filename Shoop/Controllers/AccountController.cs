eed1122cfd32fd021adb7d64dbffd27467335c5b
Jacqueline de Caluwe <jhmdec@hotmail.com>
2020-08-13 10:46:21 +0200

    Trying to solve Github issues with push/pull

--- Shoop/Controllers/AccountController.cs
+++ Shoop/Controllers/AccountController.cs
@@ -9,26 +9,20 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Shoop.Models;
-using Microsoft.AspNet.Identity.EntityFramework;
-
 
 namespace Shoop.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
-        ApplicationDbContext context = new ApplicationDbContext();
-
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
         public AccountController()
         {
-            UserManager=new ApplicationUserManager(new UserStore<ApplicationUser>(context));
-
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -40,9 +34,9 @@ namespace Shoop.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -57,7 +51,7 @@ namespace Shoop.Controllers
                 _userManager = value;
             }
         }
-       
+
         //
         // GET: /Account/Login
         [AllowAnonymous]
@@ -78,7 +72,6 @@ namespace Shoop.Controllers
             {
                 return View(model);
             }
-             SignInManager = new ApplicationSignInManager(UserManager, AuthenticationManager);
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
@@ -127,7 +120,7 @@ namespace Shoop.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -158,33 +151,21 @@ namespace Shoop.Controllers
         {
             if (ModelState.IsValid)
             {
-                //if (UserManager != null)
-                //{
-             
-                //var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-
-                //Firstname and lastname added after Nalini demo
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName=model.LastName };
-                //UserManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
-
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName};
                 var result = await UserManager.CreateAsync(user, model.Password);
-                    if (result.Succeeded)
-                    {
-                    SignInManager = new ApplicationSignInManager(UserManager, AuthenticationManager);
-
+                if (result.Succeeded)
+                {
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
-                        // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                        // Send an email with this link
-                        // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                        // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                        // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                        return RedirectToAction("Index", "Home");
-                    }
-
-                    AddErrors(result);
-                //}
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
             }
 
             // If we got this far, something failed, redisplay form
@@ -499,7 +480,6 @@ namespace Shoop.Controllers
                 context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
             }
         }
-
         #endregion
     }
 }
--- Shoop/Controllers/ManageController.cs
+++ Shoop/Controllers/ManageController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Shoop.Models;
+using Shoop.Controllers;
 
 namespace Shoop.Controllers
 {
